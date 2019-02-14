fbb7595c8819d576a6ddb9eacb503bad08dee5e2
Josh Matthews <jmatth@pm.me>
2019-02-13 19:11:46 -0500

    Treating events with and without enclosing tags the same

--- scraper/src/events/mod.rs
+++ scraper/src/events/mod.rs
@@ -4,16 +4,10 @@ use regex::Regex;
 use select::node::Node;
 use select::predicate::*;
 use std::iter::Peekable;
+use select::predicate as pred;
 
 mod matchers;
 
-const FULL_EVENT_REGEX: &str = "\
-                           ^(?P<code>[A-Z][0-9]+): \
-                           ((?P<system>.*); )?\
-                           \"(?P<title>.*)\"\
-                           ( by (?P<authors>[a-zA-Z ]+))?\
-                           (( written and|;)? presented by (?P<presenters>[a-zA-Z ]+))?. \
-                           (?P<description>.*)";
 const CODE_REGEX: &str = "^[A-Z][0-9]+$";
 
 const MATCHERS: [fn(&String) -> Option<Event>; 2] = [
@@ -56,101 +50,61 @@ where
         }
 
         // Found the start of an event
+        let body =
         if node.is(Or(Name("font"), Name("b"))) && CODE_RE.is_match(node.text().as_str()) {
+            // Event isn't wrapped in a <p> tag, usually just the first event is like this.
             match extract_flat_event(nodes) {
-                None => (),
-                Some(event) => {
-                    events.push(event);
-                }
-            };
-            continue;
-        }
-
-        if node.is(Name("p")) {
-            let body = node.text().replace('\n', "").trim().to_string();
-            match MATCHERS.into_iter().find_map(|f| f(&body)) {
+                Some(body) => body,
                 None => {
-                    println!("<{}>", body);
-                },
-                Some(event) => {
-                    events.push(event);
+                    nodes.next();
+                    continue;
                 }
             }
+        } else if node.is(Name("p")) {
+            // Most events are wrapped in a <p> tag, get its contents as text.
+            node.text().replace('\n', "").trim().to_string()
+        } else {
+            // Found something we don't know how to handle, ignore and keep going.
+            println!(
+                "Don't know how to handle node {}: {}",
+                node.index(),
+                node.html()
+            );
             nodes.next();
-            continue;
-        }
+            continue
+        };
 
-        println!(
-            "Don't know how to handle node {}: {}",
-            node.index(),
-            node.html()
-        );
+        match MATCHERS.into_iter().find_map(|f| f(&body)) {
+            None => {
+                println!("<{}>", body);
+            },
+            Some(event) => {
+                events.push(event);
+            }
+        }
         nodes.next();
     }
     println!("{}", events.len());
     events
 }
 
-fn extract_flat_event<'a, I>(nodes: &mut I) -> Option<Event>
+fn extract_flat_event<'a, I>(nodes: &mut Peekable<I>) -> Option<String>
 where
     I: Iterator<Item = Node<'a>>,
 {
-    lazy_static! {
-        static ref RE: Regex = Regex::new(FULL_EVENT_REGEX).unwrap();
-    }
     let iter_limit = 8;
     let mut event_block = String::new();
     for _ in 0..iter_limit {
+        if nodes.peek().map_or(false, |n| n.is(pred::Name("p"))) {
+            return if event_block.is_empty() { None } else { Some(event_block) };
+        }
         event_block.push_str(
-            nodes
-                .next()
+            nodes.next()
                 .map(|n| n.text())
                 .unwrap_or("".to_string())
                 .replace('\n', "")
                 .as_str(),
         );
-        let captures = match RE.captures(event_block.as_str()) {
-            None => continue,
-            Some(c) => c,
-        };
-        return Some(captures_to_event(&captures));
     }
     None
-}
-
-fn captures_to_event(captures: &Captures) -> Event {
-    let as_string = |m: Match| m.as_str().to_string();
-    let code = captures
-        .name("code")
-        .map(as_string)
-        .unwrap_or("".to_string());
-    let system = captures
-        .name("system")
-        .map(as_string)
-        .unwrap_or("".to_string());
-    let title = captures
-        .name("title")
-        .map(as_string)
-        .unwrap_or("".to_string());
-    let authors = captures
-        .name("authors")
-        .map(as_string)
-        .unwrap_or("".to_string());
-    let presenters = captures
-        .name("presenters")
-        .map(as_string)
-        .unwrap_or("".to_string());
-    let description = captures
-        .name("description")
-        .map(as_string)
-        .unwrap_or("".to_string());
-    Event {
-        code,
-        title,
-        system,
-        authors,
-        presenters,
-        description,
-        ..Default::default()
-    }
 }
