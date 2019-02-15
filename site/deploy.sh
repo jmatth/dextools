9cb4ed97228968cd4218eed2e129187c34322cd0
Josh Matthews <jmatth@pm.me>
2019-02-15 01:59:02 -0500

    Adding deploy script

---
+++ site/deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -xe
+deployDir=dist
+deployBranch=gh-pages
+buildCmd='npm run build'
+cname='dextools.jmatth.com'
+
+rm -rf $deployDir
+git worktree add $deployDir $deployBranch
+wtBackup=`mktemp`
+cp $deployDir/.git $wtBackup
+
+gitref=$(git show-ref HEAD | cut -d' ' -f1)
+rm -rf $deployDir/*
+$buildCmd
+
+mv $wtBackup $deployDir/.git
+pushd $deployDir
+echo $cname > CNAME
+git add .
+git commit -m "Updating site to ${gitref}"
+git push
+popd
+git worktree remove $deployDir
