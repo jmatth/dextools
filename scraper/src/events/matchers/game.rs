3e5579767231ff6b37040defdb1276e3da08eaf0
Joshua Matthews <josh@jmatth.com>
2019-02-19 11:09:33 -0500

    Slight regex tweaks

--- scraper/src/events/matchers/game.rs
+++ scraper/src/events/matchers/game.rs
@@ -5,7 +5,7 @@ use super::Event;
 use super::dateparse::DateParser;
 
 const EVENT_REGEX: &str = "^(?P<code>[A-Z][0-9]+): \
-                           (?P<system>[^.]+)\\. \
+                           (?P<system>.*?)\\. \
                            (?P<description>.*) \
                            (?P<time>(Wednesday|Thursday|Friday|Saturday|Sunday), [0-9]{1,2}:[0-9]{2}(AM|PM) - [0-9]{1,2}:[0-9]{2}(AM|PM))\
                            (?P<misc>.*)$";
