c85fcb3fab5f6b303e7207738580bb7a239658cd
Josh Matthews <jmatth@pm.me>
2019-02-25 00:29:46 -0500

    Removing footer

--- site/src/App.vue
+++ site/src/App.vue
@@ -82,9 +82,6 @@
         </v-layout>
       </v-container>
     </v-content>
-    <v-footer color="indigo" app>
-      <span class="white--text">&copy; 2019</span>
-    </v-footer>
   </v-app>
 </template>
 
