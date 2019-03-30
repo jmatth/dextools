c08e4020f0173db9e6b99ebdc8bec6a352aae8c8
Josh Matthews <jmatth@pm.me>
2019-03-30 12:13:07 -0400

    Updating site to 9cdbb2b8eee61a51c642c1ff65816217443d345b

---
+++ precache-manifest.cd846a56614238039967260bdcfc6670.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "fc64ea57135c4e3328ef5f0d3f1060e6",
+    "url": "/schedule.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "d68536ea0433eae244c9",
+    "url": "/js/chunk-vendors.52434a7f.js"
+  },
+  {
+    "revision": "fb8c081d10546c3f2cd4",
+    "url": "/js/app.865f4a90.js"
+  },
+  {
+    "revision": "ea02bb535b32c30e2007841c8e6036ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d68536ea0433eae244c9",
+    "url": "/css/chunk-vendors.26c50873.css"
+  },
+  {
+    "revision": "fb8c081d10546c3f2cd4",
+    "url": "/css/app.e0f0bbfc.css"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.46f61dc97670263809f4209137751c83.js"
+  "/precache-manifest.cd846a56614238039967260bdcfc6670.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "dextools"});
