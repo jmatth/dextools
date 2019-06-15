0291f4011fa048f88761a0fbe289e09ac54e9b55
Josh Matthews <josh@jmatth.com>
2019-06-15 12:46:20 -0400

    Just completely removing settings.json from the precache manifest

--- site/vue.config.js
+++ site/vue.config.js
@@ -1,50 +1,14 @@
 module.exports = {
   pwa: {
     workboxOptions: {
-      runtimeCaching: [{
-        // Match the exact path /settings.json.
-        urlPattern: /^\/settings.json$/,
-        // Apply a network-first strategy.
-        handler: 'networkFirst',
-        options: {
-          // Fall back to the cache after 5 seconds.
-          networkTimeoutSeconds: 5,
-          // Use a custom cache name for this route.
-          cacheName: 'manual-updates-cache',
-          // Configure custom cache expiration.
-          // expiration: {
-          //   maxEntries: 5,
-          //   maxAgeSeconds: 60,
-          // },
-          // Configure background sync.
-          // backgroundSync: {
-          //   name: 'manual-updates-queue',
-          //   options: {
-          //     maxRetentionTime: 60 * 60,
-          //   },
-          // },
-          // Configure which responses are considered cacheable.
-          cacheableResponse: {
-            statuses: [0, 200],
-            // headers: {'x-test': 'true'},
-          },
-          // Configure the broadcast cache update plugin.
-          // broadcastUpdate: {
-          //   channelName: 'my-update-channel',
-          // },
-          // Add in any additional plugin logic you need.
-          // plugins: [
-          //   {cacheDidUpdate: () => /* custom plugin code */}
-          // ],
-          // matchOptions and fetchOptions are used to configure the handler.
-          // fetchOptions: {
-          //   mode: 'no-cors',
-          // },
-          // matchOptions: {
-          //   ignoreSearch: true,
-          // },
-        },
-      }]
-    }
-  }
+      exclude: [
+        /\.map$/,
+        /img\/icons\//,
+        /favicon\.ico$/,
+        /^manifest.*\.js?$/,
+        /^manifest.*\.json$/,
+        /^settings\.json$/,
+      ],
+    },
+  },
 };
