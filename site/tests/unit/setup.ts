ef949e67ae9f112374c06e4c89757d83f5202094
Josh Matthews <josh@jmatth.com>
2020-01-21 18:43:25 -0500

    Adding temporary code to supress vue .native warnings

--- site/tests/unit/setup.ts
+++ site/tests/unit/setup.ts
@@ -4,6 +4,19 @@ import log from 'loglevel';
 
 Vue.use(Vuetify);
 
+// TODO: Remove this when vue and/or vuetify figure themselves out.
+const ignoreWarnMessage = 'The .native modifier for v-on is only valid on components but it was used on <div>.';
+Vue.config.warnHandler = function(msg, vm, trace) {
+  if (msg === ignoreWarnMessage) {
+    // @ts-ignore
+    msg = null;
+    // @ts-ignore
+    vm = null;
+    // @ts-ignore
+    trace = null;
+  }
+};
+
 declare const global: any;
 global.requestAnimationFrame = (cb: any) => cb();
 global.localStorage = window.localStorage;
