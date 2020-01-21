88f04e6e8adaf7ba1b78c5e0ee99765d4532a2c3
Josh Matthews <josh@jmatth.com>
2020-01-21 18:43:03 -0500

    Adding many more AgendaCalendar unit tests

--- site/tests/unit/components/AgendaCalendar.spec.ts
+++ site/tests/unit/components/AgendaCalendar.spec.ts
@@ -38,6 +38,11 @@ describe('components/AgendaCalendar.vue', () => {
           start_time: '2020-11-02 09:00',
           end_time: '2020-11-02 10:00',
         }),
+        F003: createEvent({
+          code: 'F003',
+          start_time: '2020-11-03 09:00',
+          end_time: '2020-11-03 10:00',
+        }),
       },
     };
     store = new Vuex.Store({
@@ -57,16 +62,108 @@ describe('components/AgendaCalendar.vue', () => {
       });
     });
 
+    it('computes the start and end dates correctly', () => {
+      // @ts-ignore
+      expect(wrapper.vm.startCal).to.equal('2020-11-01');
+      // @ts-ignore
+      expect(wrapper.vm.endCal).to.equal('2020-11-03');
+    });
+
     it('defaults to day view', () => {
-      expect(wrapper.vm.calType).to.equal('day');
       expect(wrapper.findAll('.v-calendar .v-calendar-daily__day').length).to.equal(1);
+    });
+
+    it('has the "Day" button selected', () => {
       expect(wrapper.findAll('.v-btn-toggle .v-btn').at(0).classes('v-btn--active')).to.be.true;
       expect(wrapper.findAll('.v-btn-toggle .v-btn').at(1).classes('v-btn--active')).to.be.false;
+    });
+
+    it('starts on the first day', () => {
+      expect(wrapper.get('.v-calendar-daily_head-weekday').text()).to.equal('Sun 1');
     });
 
     it('shows the day switcher buttons', () => {
       const buttons = wrapper.findAll('.v-toolbar__content > .v-btn');
       expect(buttons.length).to.equal(2);
+    });
+
+    it('disables the back day switcher button', () => {
+      const buttons = wrapper.findAll('.v-toolbar__content > .v-btn');
+      const backButton = buttons.at(0);
+      expect(backButton.attributes('disabled')).to.equal('disabled');
+    });
+
+    it('does not disable the forward day switcher button', () => {
+      const buttons = wrapper.findAll('.v-toolbar__content > .v-btn');
+      const forwardButton = buttons.at(1);
+      expect(forwardButton.attributes('disabled')).to.not.exist;
+    });
+
+    describe('step forward one day', () => {
+      beforeEach(() => {
+        const buttons = wrapper.findAll('.v-toolbar__content > .v-btn');
+        const forwardButton = buttons.at(1);
+        forwardButton.trigger('click');
+        return wrapper.vm.$nextTick();
+      });
+
+      it('advances to the next day', () => {
+        expect(wrapper.get('.v-calendar-daily_head-weekday').text()).to.equal('Mon 2');
+      });
+
+      it('does not disable either day switcher button', () => {
+        const buttons = wrapper.findAll('.v-toolbar__content > .v-btn');
+        const backButton = buttons.at(1);
+        const forwardButton = buttons.at(1);
+        expect(backButton.attributes('disabled')).to.not.exist;
+        expect(forwardButton.attributes('disabled')).to.not.exist;
+      });
+
+      describe('step to final day', () => {
+        beforeEach(() => {
+          const buttons = wrapper.findAll('.v-toolbar__content > .v-btn');
+          const forwardButton = buttons.at(1);
+          forwardButton.trigger('click');
+          return wrapper.vm.$nextTick();
+        });
+
+        it('advances to the next day', () => {
+          expect(wrapper.get('.v-calendar-daily_head-weekday').text()).to.equal('Tue 3');
+        });
+
+        it('does not disable the back day switcher button', () => {
+          const buttons = wrapper.findAll('.v-toolbar__content > .v-btn');
+          const backButton = buttons.at(0);
+          expect(backButton.attributes('disabled')).to.not.exist;
+        });
+
+        it('disables the forward day switcher button', () => {
+          const buttons = wrapper.findAll('.v-toolbar__content > .v-btn');
+          const forwardButton = buttons.at(1);
+          expect(forwardButton.attributes('disabled')).to.equal('disabled');
+        });
+
+        describe('step back one day', () => {
+          beforeEach(() => {
+            const buttons = wrapper.findAll('.v-toolbar__content > .v-btn');
+            const backButton = buttons.at(0);
+            backButton.trigger('click');
+            return wrapper.vm.$nextTick();
+          });
+
+          it('moves to the previous day', () => {
+            expect(wrapper.get('.v-calendar-daily_head-weekday').text()).to.equal('Mon 2');
+          });
+
+          it('does not disable either day switcher button', () => {
+            const buttons = wrapper.findAll('.v-toolbar__content > .v-btn');
+            const backButton = buttons.at(1);
+            const forwardButton = buttons.at(1);
+            expect(backButton.attributes('disabled')).to.not.exist;
+            expect(forwardButton.attributes('disabled')).to.not.exist;
+          });
+        });
+      });
     });
   });
 
@@ -85,10 +182,17 @@ describe('components/AgendaCalendar.vue', () => {
     });
 
     it('renders the multi-day view', () => {
-      expect(wrapper.vm.calType).to.equal('custom-daily');
-      expect(wrapper.findAll('.v-calendar .v-calendar-daily__day').length).to.be.equal(2);
+      expect(wrapper.findAll('.v-calendar .v-calendar-daily__day').length).to.be.equal(3);
+    });
+
+    it('has the "Week" button selected', () => {
       expect(wrapper.findAll('.v-btn-toggle .v-btn').at(0).classes('v-btn--active')).to.be.false;
       expect(wrapper.findAll('.v-btn-toggle .v-btn').at(1).classes('v-btn--active')).to.be.true;
+    });
+
+    it('does not show the day switcher buttons', () => {
+      const buttons = wrapper.findAll('.v-toolbar__content > .v-btn');
+      expect(buttons.length).to.equal(0);
     });
   });
 });
