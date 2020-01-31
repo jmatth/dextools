129050b037a970198389c3351e197104683bca09
Joshua Matthews <josh@jmatth.com>
2020-01-31 00:49:53 -0500

    Updating help dialog
    Closes #46

--- site/src/components/AboutDialog.vue
+++ site/src/components/AboutDialog.vue
@@ -19,23 +19,27 @@
     </template>
     <template>
       <h2>What's this?</h2>
-      <p>
+      <p class="text-justify">
       This is a site to help with building your schedule for the excellent conventions run by Double Exposure Inc.
       It uses data scraped from the Double Exposure website so may be innacurrate or out of date.
       </p>
       <h2>How can I build my convention schedule?</h2>
-      <p>
-      Use the <b>+</b> buttons in the event list to add an event to your schedule. You can see your current schedule in the calendar view.
-      To remove an event from your schedule, just click its entry in the calendar. When you are satisfied with your schedule, you can click
-      the <b>Export</b> button at the top of the calendar to export your schedule to an email to sign up for events, or to a format for importing
-      into calendar applications like Google Calendar.
+      <p class="text-justify">
+      Use the
+      <v-icon small>add_circle</v-icon>
+      and
+      <v-icon small>remove_circle</v-icon>
+      buttons in the event list to add or remove an event from your schedule.
+      You can see your current schedule in the calendar view. Orange colored
+      calendar events indicate a conflict. You can click on a calendar event to
+      view more information about it or remove it from your schedule.
       <b>
-        Simply adding events to your schedule will not register you for those events, you must still send email to the
-        appropriate email address for the convention. This site will just help you produce the text for that email.
+        Simply adding events to your schedule will not register you for those
+        events, you must still send email to the appropriate email address for
+        the convention (see next the "How do I use the export page?" section).
       </b>
-      </p>
       <h2>How do I use the search features?</h2>
-      <p>
+      <p class="text-justify">
       It is possible to filter the events list by several criteria:
       <ul>
         <li>
@@ -48,7 +52,7 @@
         </li>
       </ul>
       By clicking the
-      <v-icon dense aria-label="Advanced filters">filter_list</v-icon>
+      <v-icon small aria-label="Advanced filters">filter_list</v-icon>
       icon, you can view advanced search options:
       <ul>
         <li>
@@ -68,13 +72,26 @@
         </li>
       </ul>
       </p>
+      <h2>How do I use the export page?</h2>
+      <p class="text-justify">
+        When you are satisfied with your schedule, you can navigate to the
+        <b>Export</b> page to sign up for events or export your schedule as
+        calendar events. To sign up for events, enter your name and copy the
+        generated text, then send it to the signup email for the convention. If
+        you have an email client configured you can also click the <b>Email</b>
+        button to open your email client with a pre-generated email including the
+        message body, subject, and to address. To export your schedule as
+        calendar events, click the <b>ICS</b> button and download the resulting
+        file, then import it into your calendar program of choice (Google
+        Calendar, MacOS Calendar, etc).
+      </p>
       <h2>Questions/bug reports/feedback?</h2>
-      <p>
+      <p class="text-justify">
       Any feedback via <a :href="this.$store.state.feedbackUrl" target="_blank">this anonymous form</a> is greatly appreciated.
       If you are so inclined, you can also open issues directly <a href="https://github.com/jmatth/dextools/issues/new" target="_blank">on the github repo</a>.
       </p>
       <h2>I should probably add...</h2>
-      <p>
+      <p class="text-justify">
       This site is not associated with or endorsed by Double Exposure Inc.
       </p>
     </template>
