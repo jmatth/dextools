8efac5e9a89c055ccfb436d0a1bded4f8f8e0fd8
Josh Matthews <josh@jmatth.com>
2020-02-02 21:30:00 -0500

    Updating auto deploy scripts

--- deploy/alert.sh
+++ deploy/alert.sh
@@ -5,5 +5,5 @@ cd "$(dirname "$0")"
 num_events=$(< ./settings.json jq '.schedule | length')
 
 while read number; do
-  ./send-sms.sh "${number}" "https://dextools.jmatth.com updated with ${num_events} events!"
+  ./send-sms.sh "${number}" "https://dextools.app updated with ${num_events} events!"
 done < numbers.txt
--- deploy/auto-update.sh
+++ deploy/auto-update.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
 
+WORKTREE_DIR="gh-pages"
+
 if [ "$#" -ne 3 ]; then
   echo "Usage: $0 URL BACKUP_URL YYYY-mm-dd"
   exit 1
@@ -15,23 +17,30 @@ panic() {
 }
 trap panic ERR
 
+if ! test -d $WORKTREE_DIR; then
+  echo "Initializing worktree at $WORKTREE_DIR"
+  git fetch
+  git worktree add $WORKTREE_DIR gh-pages
+fi
+
 echo '=================================='
 
 dexposure-scraper -d "$3" -i "$1" -t settings_primary.json -o settings.json &> /dev/null || \
 dexposure-scraper -d "$3" -i "$2" -t settings_backup.json  -o settings.json &> /dev/null
 
-if /usr/bin/diff -q settings.json ../site/dist/settings.json; then
+if /usr/bin/diff -q settings.json $WORKTREE_DIR/settings.json; then
   echo "No changes detected at $(date)."
   exit 0
 fi
 echo 'Changes detected, updating...'
 
-cp settings.json ../site/dist/settings.json
-
-pushd ../site/dist &> /dev/null
+pushd $WORKTREE_DIR &> /dev/null
+git fetch
+git reset --hard origin/gh-pages
+cp ../settings.json settings.json
 
 git commit -am "Auto update settings at $(date)."
-git push -f
+git push
 echo "Updated at $(date)."
 
 popd &> /dev/null
